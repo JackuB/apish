80f14f8f86e6371173b6701be920d1e275801124
Jakub Mikulas <jakub@mikul.as>
2016-01-20 17:46:51 +0100

    Updated description

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,10 @@
 var webpack = require('webpack');
-var minimize = process.argv.indexOf('--no-minimize') === -1 ? true : false;
-var plugins = minimize
-  ? [new webpack.optimize.UglifyJsPlugin({ minimize: true })]
-  : [];
 
 module.exports = {
   entry: './src/apish.js',
   output: {
     path: './dist',
-    filename: minimize ? 'apish.min.js' : 'apish.js',
+    filename: 'apish.js',
     libraryTarget: 'umd',
     library: 'apish'
   },
@@ -18,6 +14,5 @@ module.exports = {
       exclude: /(node_modules|bower_components)/,
       loader: 'babel'
     }]
-  },
-  plugins: plugins
+  }
 };
