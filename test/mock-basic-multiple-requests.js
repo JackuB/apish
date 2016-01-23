08cd1d6518a9ff35397f4de4979bbc728facc7f0
Jakub Mikulas <jakub@mikul.as>
2016-01-24 00:46:41 +0100

    Added timeout to npm run coverage

--- test/mock-basic-multiple-requests.js
+++ test/mock-basic-multiple-requests.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import nock from 'nock';
 import hippie from 'hippie';
 import apish from '../src/apish.js';
+import { expect } from 'chai'; // coverage runner fix
 
 describe('Mock basic APIB with multiple requests and 1 response', () => {
   let mockResult = {};
