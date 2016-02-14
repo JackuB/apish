20bfbef24ca4528c8836fae0880fd39608295d88
Jakub Mikulas <jakub@mikul.as>
2016-02-14 21:19:52 +0100

    Updated deps, testing for swagger bodies again

--- test/mock-basic-swagger.js
+++ test/mock-basic-swagger.js
@@ -1,18 +1,15 @@
 import fs from 'fs';
-import nock from 'nock';
 import hippie from 'hippie';
 import apish from '../src/apish.js';
 
 describe('Mock basic Swagger', () => {
   let mockResult = {};
   before(() => {
-    nock.disableNetConnect('apiblueprint.org');
     const apib = fs.readFileSync(__dirname + '/fixtures/basic-swagger.yaml');
     return mockResult = apish(apib.toString());
   });
 
   after(() => {
-    nock.enableNetConnect('apiblueprint.org');
     mockResult.value().restore();
   });
 
@@ -22,9 +19,8 @@ describe('Mock basic Swagger', () => {
     .base('http://petstore.swagger.io')
     .get('/api/pets')
     .expectStatus(200)
-    //TODO: we need refract body generator
-    //.expectValue('[0].name', /\S+/)
-    //.expectValue('[0].id', /\d+/)
+    .expectValue('[0].name', /\S+/)
+    .expectValue('[0].id', /\d+/)
     .end(done);
   });
 });
